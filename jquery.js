9d7a7512d5b85de33408b88646d8ddfbc8123d9e
yamasakiyutaka <theworldfinishes@gmail.com>
2016-02-26 16:32:03 +0900

    jquery 実践　2/26

---
+++ jquery.js
@@ -0,0 +1,17 @@
+$(function(){
+
+
+
+
+	
+	// もっと詳しくの詳細 
+	$(".aicon").hover(
+		function(){
+
+	$(this).find('.hove').addClass('hove-active');
+	},
+	function(){
+	$(this).find('.hove').removeClass('hove-active');}
+	);
+
+		});
