553cc9c93390407e0fa1d65be4b35f7b773f4110
yamasakiyutaka <theworldfinishes@gmail.com>
2016-02-28 00:11:20 +0900

    jquery開発　2/28

--- jquery.js
+++ jquery.js
@@ -1,17 +1,28 @@
 $(function(){
+	// console.log("test")
+ //   // #で始まるアンカーをクリックした場合に処理
+ //   $('a[href^=#]').click(function() {
+ //      // スクロールの速度
+ //      var speed = 400; // ミリ秒
+ //      // アンカーの値取得
+ //      var href= $(this).attr("href");
+ //      // 移動先を取得
+ //      var target = $(href == "#" || href == "" ? 'html' : href);
+ //      // 移動先を数値で取得
+ //      var position = target.offset().top;
+ //      // スムーススクロール
+ //      $('body,html').animate({scrollTop:position}, speed, 'swing');
+ //      return false;
+ //   });
+ $('.word-box-hover').hover(
+ 	function() {
+ 		$(this).find('.text').fadeIn();
+ },
+    function(){
+ 	    $(this).find('.text').fadeOut();
+ }
 
 
+ );
 
-
-	
-	// もっと詳しくの詳細 
-	$(".aicon").hover(
-		function(){
-
-	$(this).find('.hove').addClass('hove-active');
-	},
-	function(){
-	$(this).find('.hove').removeClass('hove-active');}
-	);
-
-		});
+});
